fc5daf52a44d2cb374e03b3e9ec4f767075db678
Sagar <sagar@local>
2026-03-20 17:14:20 +0700

    exp53: TIME_BUDGET 600->1200s (20 min training)

--- prepare.py
+++ prepare.py
@@ -36,7 +36,7 @@ from mlx.utils import tree_flatten, tree_unflatten
 # ---------------------------------------------------------------------------
 
 MAX_SEQ_LEN = 1024          # context length (matches baseline)
-TIME_BUDGET = int(os.environ.get("TIME_BUDGET", 600))  # training time budget in seconds (10 min)
+TIME_BUDGET = int(os.environ.get("TIME_BUDGET", 1200))  # training time budget in seconds (20 min)
 MAX_COMPRESSED_BYTES = 16_000_000  # 16 MB compressed artifact limit
 
 # ---------------------------------------------------------------------------
